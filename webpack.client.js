a8d3864948e72c5a58a49f6dfffc5b6e5ca8dd4b
Mohammadreza Zarei <zarei.mohamadreza@gmail.com>
2018-08-06 17:37:30 +0200

    import Environment variables

--- webpack.client.js
+++ webpack.client.js
@@ -1,7 +1,7 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const webpack = require('webpack');
 module.exports = {
   entry: {
     main: "./src/index.js"
@@ -11,6 +11,10 @@ module.exports = {
     path: path.resolve("dist/client")
   },
   plugins: [
+    new webpack.DefinePlugin({
+      LAMBDA_ENDPOINT: JSON.stringify(process.env.LAMBDA_ENDPOINT),
+      STRIPE_PUBLISHABLE_KEY: JSON.stringify(process.env.STRIPE_PUBLISHABLE_KEY),
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html"
     })
